ee0e579a58cdd5020e21ef5ecaa0d3b07d21cbce
GustavoRodriguesk <gustavorodrig780@gmail.com>
2024-08-06 17:54:10 -0300

    criando rotas de teste

--- src/server.js
+++ src/server.js
@@ -1,14 +1,39 @@
-//const express = require(express)
-import express from "express";
+import express from 'express';
+const app = express();
 
-const app = express()
+app.use(express.json());
 
-app.get('/', (req, res) => {
-res.json({
-    message: "vamos pra cima express",
-    success: "Executou Bonito"
-})
-})
+app.post('/auth/signup', (req, res) => {
+    res.json({ message: "rota de POST auth/signup" });
+});
+
+app.post('/auth/login', (req, res) => {
+    res.json({ message: "rota de POST login" });
+});
+
+app.post('/auth/logout', (req, res) => {
+    res.json({ message: "rota de POST logout" });
+});
+
+app.post('/account', (req, res) => {
+    res.json({ message: "rota de POST account" });
+});
+app.get('/account/list', (req, res) => {
+    res.json({ message: "rota de GET list" });
+});
+
+app.get('/account/:id', (req, res) => {
+    res.json({ message: "rota de GET account id" });
+});
+
+app.put('/account/:id', (req, res) => {
+    res.json({ message: "rota de PUT account id" });
+});
+
+app.delete('/account/:id', (req, res) => {
+    res.json({ message: "rota de DELETE account id" });
+});
+
 app.listen(3000, () => {
-    console.log('servidor rodando na porta http://localhost:3000')
-})
+    console.log('Servidor rodando em http://localhost:3000');
+});
